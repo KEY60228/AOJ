0e6c816b8da6eaea1822f39776d1144fa397397b
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-12-23 23:41:01 +0900

    9-2. 二分探索木: 挿入

---
+++ Courses/Lesson/ALDS1/8/A/main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+// #include <fstream>
+
+using namespace std;
+
+struct Node {
+    int key;
+    Node *parent, *left, *right;
+};
+
+Node *root, *NIL;
+
+void insert(int k) {
+    Node *y = NIL;
+    Node *x = root;
+    Node *z;
+
+    z = new Node();
+    z->key = k;
+    z->left = NIL;
+    z->right = NIL;
+
+    while (x != NIL) {
+        y = x;
+        if (z->key < x->key) {
+            x = x->left;
+        } else {
+            x = x->right;
+        }
+    }
+
+    z->parent = y;
+    if (y == NIL) {
+        root = z;
+    } else {
+        if (z->key < y->key) {
+            y->left = z;
+        } else {
+            y->right = z;
+        }
+    }
+}
+
+void inorder(Node *u) {
+    if (u == NIL) return;
+    inorder(u->left);
+    cout << " " << u->key;
+    inorder(u->right);
+}
+
+void preorder(Node *u) {
+    if (u == NIL) return;
+    cout << " " << u->key;
+    preorder(u->left);
+    preorder(u->right);
+}
+
+int main() {
+    // ifstream in("input.txt");
+    // cin.rdbuf(in.rdbuf());
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        string com;
+        cin >> com;
+        if (com == "insert") {
+            int x;
+            cin >> x;
+            insert(x);
+        } else if (com == "print") {
+            inorder(root);
+            cout << endl;
+            preorder(root);
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
